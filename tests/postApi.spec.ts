ff18224f6f611798363183f4d9772bd190c17a41
Raj-Kumar13 <rajcheeyan@gmail.com>
2026-06-04 11:10:43 -0700

    Code-fix: Refactor API call function and update test cases for improved response handling

--- tests/postApi.spec.ts
+++ tests/postApi.spec.ts
@@ -1,20 +1,28 @@
 
 import { test, expect } from "@playwright/test";
-import { posthttpCall } from "../api/httpCalls";
+import { postHttpCall } from "../api/httpCalls";
 import manager from "../env/manager";
 import data from "../test-data/login/success.json";
 
 test.describe("Testing of all API calls", () => {
 
     test(" Testing of POST API Call", async () => {
-
-
-        const response:any = await posthttpCall(`${manager.baseUrl}/auth/login`, 
+        const response: any = await postHttpCall(
+            `${manager.baseUrl}/auth/login`,
             data
         );
-        expect(response.status).toBe(200);
-
-        console.log("Response Body: ", response.body);
+        expect(response.status).toBe(201);
+        expect(response.body).toMatchObject({
+            success: true,
+            token: 'jwt-token-123',
+            refreshToken: 'refresh-token-456',
+            user: {
+                id: 101,
+                name: 'John Smith',
+                email: 'john@test.com',
+                role: 'customer'
+            }
+        });
     })
 
 });
